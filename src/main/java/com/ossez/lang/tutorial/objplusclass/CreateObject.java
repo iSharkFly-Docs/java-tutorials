cab35e0dd2a8abb333b31dbd1bfc8b1c89be920a
YuCheng Hu <yuchenghu@ossez.com>
2018-11-21 15:04:47 -0500

    Java 使用构造方法创建对象

---
+++ src/main/java/com/ossez/lang/tutorial/objplusclass/CreateObject.java
@@ -0,0 +1,18 @@
+package com.ossez.lang.tutorial.objplusclass;
+
+/**
+ * 
+ * @author YuCheng
+ *
+ */
+public class CreateObject {
+	public CreateObject(String name) {
+		// This constructor has one parameter, name
+		System.out.println("小狗的名字是: " + name);
+	}
+
+	public static void main(String[] args) {
+		// Following statement would create an object myPuppy
+		CreateObject myPuppy = new CreateObject("Tomcat");
+	}
+}
