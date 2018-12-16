1fecd0a70aef1a653e1d47b0605170b48cabb540
Yucheng Hu <yuchenghu@ossez.com>
2018-12-16 14:10:23 -0500

    二叉树的序列化和反序列化

---
+++ src/main/java/com/ossez/lang/tutorial/models/TreeNode.java
@@ -0,0 +1,16 @@
+package com.ossez.lang.tutorial.models;
+
+/**
+ * 
+ * @author YuCheng
+ *
+ */
+public class TreeNode {
+	public int val;
+	public TreeNode left, right;
+
+	public TreeNode(int val) {
+		this.val = val;
+		this.left = this.right = null;
+	}
+}
