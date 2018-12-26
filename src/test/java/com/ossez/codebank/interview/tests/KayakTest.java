79296322bf1008f5da08bea48a6bcfd4feec0d78
YuCheng Hu <yuchenghu@ossez.com>
2018-12-25 21:20:50 -0500

    不使用循环的输出测试程序

--- src/test/java/com/ossez/codebank/interview/tests/KayakTest.java
+++ src/test/java/com/ossez/codebank/interview/tests/KayakTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ossez.codebank.interview.KayakCountUpDown;
 import com.ossez.codebank.interview.KayakRobotMovement;
 
 /**
@@ -18,7 +19,10 @@ public class KayakTest {
 	private final static Logger logger = LoggerFactory.getLogger(KayakTest.class);
 
 	/**
+	 * https://www.cwiki.us/display/ITCLASSIFICATION/Robot+Movement
+	 * 
 	 * testGetCoordinates
+	 * 
 	 */
 	@Test
 	public void testGetCoordinates() {
@@ -28,6 +32,32 @@ public class KayakTest {
 
 		assertEquals(KayakRobotMovement.getCoordinates("FF"), "0,2");
 		assertEquals(KayakRobotMovement.getCoordinates("LFFFRFFFRRFFF"), "-3,0");
+
+	}
+
+	/**
+	 * https://www.cwiki.us/display/ITCLASSIFICATION/Count+Up+Down
+	 * 
+	 * CountUpDown
+	 */
+	@Test
+	public void testCountUpDown() {
+		logger.debug("TEST Count Up and Down ");
+
+		// 2 -5
+		logger.debug("[2 -> 5]");
+		logger.debug("UP - {}", new KayakCountUpDown().countUp(2, 5));
+		logger.debug("UP & DOWN - {}", new KayakCountUpDown().countUpDown(2, 5));
+
+		// 0 - 5
+		logger.debug("[0 -> 5]");
+		logger.debug("UP - {}", new KayakCountUpDown().countUp(0, 5));
+		logger.debug("UP & DOWN - {}", new KayakCountUpDown().countUpDown(0, 5));
+
+		// -1 - 5
+		logger.debug("[-1 -> 5]");
+		logger.debug("UP - {}", new KayakCountUpDown().countUp(-1, 5));
+		logger.debug("UP & DOWN - {}", new KayakCountUpDown().countUpDown(-1, 5));
 
 	}
 
