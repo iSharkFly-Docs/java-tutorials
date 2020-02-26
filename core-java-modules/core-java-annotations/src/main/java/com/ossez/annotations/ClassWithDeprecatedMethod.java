b2d9b2c1d26bde4108cf6366695e2316dd5002d4
YuCheng Hu <yhu@ossez.com>
2020-02-26 15:44:44 -0500

    add new method to guide javadoc to show helps

--- core-java-modules/core-java-annotations/src/main/java/com/ossez/annotations/ClassWithDeprecatedMethod.java
+++ core-java-modules/core-java-annotations/src/main/java/com/ossez/annotations/ClassWithDeprecatedMethod.java
@@ -1,9 +1,23 @@
 package com.ossez.annotations;
 
+
 class ClassWithDeprecatedMethod {
 
+    /**
+     * Calculate period between versions
+     * * @deprecated
+     * * This method is no longer acceptable to compute time between versions.
+     * * <p> Use {@link ClassWithDeprecatedMethod#updatedMethod()} instead.
+     */
     @Deprecated
     static void deprecatedMethod() {
 
     }
+
+    /**
+     * Updated Method instead of deprecatedMethod.
+     */
+    static void updatedMethod() {
+    }
+
 }
