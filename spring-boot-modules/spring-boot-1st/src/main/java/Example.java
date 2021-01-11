637fcb9c162f0d10cd2cc2518dacc0ec62e82375
YuCheng Hu <yuchenghu@ossez.com>
2021-01-11 09:08:36 -0500

    更新源代码的 import

--- spring-boot-modules/spring-boot-1st/src/main/java/Example.java
+++ spring-boot-modules/spring-boot-1st/src/main/java/Example.java
@@ -1,7 +1,7 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
