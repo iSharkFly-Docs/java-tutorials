387d88d36ce22fe8f6e07fd0d28a957e3a8e08c6
YuCheng Hu <huyuchengus@gmail.com>
2021-09-29 13:46:08 -0400

    Add docs for xml process

--- parent-java/java-numbers-2/src/test/java/com/ossez/lossyconversion/ConversionTechniquesUnitTest.java
+++ parent-java/java-numbers-2/src/test/java/com/ossez/lossyconversion/ConversionTechniquesUnitTest.java
@@ -9,7 +9,7 @@ public class ConversionTechniquesUnitTest {
 
     @Test
     public void testPrimitiveConversion() {
-        
+
         long longNum = 24;
         short shortNum = (short) longNum;
         assertEquals(24, shortNum);
@@ -17,19 +17,19 @@ public class ConversionTechniquesUnitTest {
         double doubleNum = 15.6;
         int integerNum = (int) doubleNum;
         assertEquals(15, integerNum);
-        
+
         long largeLongNum = 32768;
         short minShortNum = (short) largeLongNum;
         assertEquals(-32768, minShortNum);
-        
+
         long smallLongNum = -32769;
         short maxShortNum = (short) smallLongNum;
         assertEquals(32767, maxShortNum);
-        
+
         long maxLong = Long.MAX_VALUE;
         int minInt = (int) maxLong;
         assertEquals(-1, minInt);
-        
+
         long minLong = Long.MIN_VALUE;
         int maxInt = (int) minLong;
         assertEquals(0, maxInt);
@@ -54,15 +54,19 @@ public class ConversionTechniquesUnitTest {
         long longNum = Math.round(doubleNum);
         assertEquals(16, longNum);
     }
-    
+
     @Test
     public void testWrapperConversion() {
 
-        Double doubleNum = 10.3;
-        double dbl = doubleNum.doubleValue(); //unboxing
-        int intgr = (int) dbl; //downcasting
-        Integer intNum = Integer.valueOf(intgr);
-        assertEquals(Integer.valueOf(10), intNum);
+//        Double doubleNum = 10.3;
+//        double dbl = doubleNum.doubleValue(); //unboxing
+//        int intgr = (int) dbl; //downcasting
+//        Integer intNum = Integer.valueOf(intgr);
+//        assertEquals(Integer.valueOf(10), intNum);
+
+
+        float floatNum = 10.12f;
+        long longNum = (long) floatNum;
     }
-    
+
 }
