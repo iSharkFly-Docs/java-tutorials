0a88382aefdebdf54e6f3640e9b7eaa6f443e574
YuCheng Hu <huyuchengus@gmail.com>
2022-03-05 11:15:16 -0500

    Add logic for JSON to OBJ

---
+++ jackson-simple/src/test/java/com/ossez/jackson/jsonproperty/JsonPropertyUnitTest.java
@@ -0,0 +1,39 @@
+package com.ossez.jackson.jsonproperty;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonPropertyUnitTest {
+
+    @Test
+    public final void whenSerializing_thenCorrect() throws JsonParseException, IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final String dtoAsString = mapper.writeValueAsString(new MyDto());
+
+        assertThat(dtoAsString, containsString("intValue"));
+        assertThat(dtoAsString, containsString("stringValue"));
+        assertThat(dtoAsString, containsString("booleanValue"));
+    }
+
+    @Test
+    public final void givenNameOfFieldIsChangedViaAnnotationOnGetter_whenSerializing_thenCorrect() throws JsonParseException, IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final MyDtoFieldNameChanged dtoObject = new MyDtoFieldNameChanged();
+        dtoObject.setStringValue("a");
+
+        final String dtoAsString = mapper.writeValueAsString(dtoObject);
+
+        assertThat(dtoAsString, not(containsString("stringValue")));
+        assertThat(dtoAsString, containsString("strVal"));
+        System.out.println(dtoAsString);
+    }
+
+}
