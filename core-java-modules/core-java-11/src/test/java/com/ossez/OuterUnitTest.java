93611de36d7fc54f2ff55d60b5438975e72fb355
YuCheng Hu <huyuchengus@gmail.com>
2022-05-13 14:44:21 -0400

    Rename the package: <module>core-java-11-</module> to project

---
+++ core-java-modules/core-java-11/src/test/java/com/ossez/OuterUnitTest.java
@@ -0,0 +1,48 @@
+package com.ossez;
+
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.ossez.Outer;
+import org.junit.Test;
+
+public class OuterUnitTest {
+
+    private static final String NEST_HOST_NAME = "Outer";
+
+    @Test
+    public void whenGetNestHostFromOuter_thenGetNestHost() {
+        is(Outer.class.getNestHost().getName()).equals(NEST_HOST_NAME);
+    }
+
+    @Test
+    public void whenGetNestHostFromInner_thenGetNestHost() {
+        is(Outer.Inner.class.getNestHost().getName()).equals(NEST_HOST_NAME);
+    }
+
+    @Test
+    public void whenCheckNestmatesForNestedClasses_thenGetTrue() {
+        is(Outer.Inner.class.isNestmateOf(Outer.class)).equals(true);
+    }
+
+    @Test
+    public void whenCheckNestmatesForUnrelatedClasses_thenGetFalse() {
+        is(Outer.Inner.class.isNestmateOf(Outer.class)).equals(false);
+    }
+
+    @Test
+    public void whenGetNestMembersForNestedClasses_thenGetAllNestedClasses() {
+        Set<String> nestMembers = Arrays.stream(Outer.Inner.class.getNestMembers())
+          .map(Class::getName)
+          .collect(Collectors.toSet());
+
+        is(nestMembers.size()).equals(2);
+
+        assertTrue(nestMembers.contains("Outer"));
+        assertTrue(nestMembers.contains("Outer$Inner"));
+    }
+}
---
+++ core-java-modules/core-java-11/src/test/java/com/ossez/benchmark/IntegerListFilterUnitTest.java
@@ -0,0 +1,34 @@
+package com.ossez.benchmark;
+
+import com.ossez.benchmark.IntegerListFilter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class IntegerListFilterUnitTest {
+
+    private IntegerListFilter integerListFilter;
+
+    @Before
+    public void init() {
+        integerListFilter = new IntegerListFilter();
+        integerListFilter.setup();
+    }
+
+    @Test
+    public void whenBenchmarkIsExecute_thenJDKListsMustBeOfSameSize() {
+        assertEquals(integerListFilter.jdkList().size(), integerListFilter.jdkListParallel().size());
+    }
+
+    @Test
+    public void whenBenchmarkIsExecute_thenMutableListsMustBeOfSameSize() {
+        assertEquals(integerListFilter.ecMutableList().size(), integerListFilter.ecMutableListParallel().size());
+    }
+
+    @Test
+    public void whenBenchmarkIsExecute_thenPrimitiveListsMustBeOfSameSize() {
+        assertEquals(integerListFilter.ecPrimitive().size(), integerListFilter.ecPrimitiveParallel().size());
+    }
+}
