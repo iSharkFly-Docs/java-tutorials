b170ec04bba17e7e5061f8c091580b536b3abe00
YuCheng Hu <huyuchengus@gmail.com>
2022-06-09 01:24:30 -0400

    修改对象成使用的 String 字符串对象来进行说明

--- core-java-modules/core-java-strings/README.md
+++ core-java-modules/core-java-strings/README.md
@@ -1,8 +1,8 @@
-## Java Strings
+## Java 字符串（Strings）
 
 This module contains articles about strings in Java.
 
-### Relevant Articles:
+### 相关文章
 - [Use char[] Array over a String for Manipulating Passwords in Java?](https://www.baeldung.com/java-storing-passwords)
 - [Compact Strings in Java 9](https://www.baeldung.com/java-9-compact-string)
 - [String Not Empty Test Assertions in Java](https://www.baeldung.com/java-assert-string-not-empty)
@@ -13,3 +13,4 @@ This module contains articles about strings in Java.
 - [Java Multi-line String](https://www.baeldung.com/java-multiline-string)
 - [Guide to Java String Pool](https://www.baeldung.com/java-string-pool)
 - [Fixing “constant string too long” Build Error](https://www.baeldung.com/java-constant-string-too-long-error)
+- [Java 对象如何安全的 toString](https://www.ossez.com/t/java-tostring/14000)
--- core-java-modules/core-java-strings/src/test/java/com/ossez/interview/StringNullCheckUnitTest.java
+++ core-java-modules/core-java-strings/src/test/java/com/ossez/interview/StringNullCheckUnitTest.java
@@ -9,17 +9,24 @@ import java.util.Objects;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
+/**
+ * Java toString null Check
+ *
+ * <p><a href="https://www.ossez.com/t/java-tostring/14000">https://www.ossez.com/t/java-tostring/14000</a></p>
+ *
+ * @author YuCheng Hu
+ */
 public class StringNullCheckUnitTest {
     @Test
     public void toString_null_safe() {
-        Object obj = null;
+        String obj = null;
 
         ObjectUtils.toString(obj, StringUtils.EMPTY);
-        ObjectUtils.toString(obj, ()->StringUtils.EMPTY);
+        ObjectUtils.toString(obj, () -> StringUtils.EMPTY);
         assertThat("null").isEqualTo(Objects.toString(obj));
-        assertThat("").isEqualTo(Objects.toString(obj,StringUtils.EMPTY));
+        assertThat("").isEqualTo(Objects.toString(obj, StringUtils.EMPTY));
 
-        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() ->{
+        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> {
             obj.toString();
         });
 
