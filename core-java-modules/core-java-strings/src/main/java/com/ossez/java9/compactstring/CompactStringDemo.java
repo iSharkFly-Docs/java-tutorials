b9bf30f350e54efe02c4f2ac06dc1001f38eea18
YuCheng Hu <huyuchengus@gmail.com>
2022-07-05 10:14:24 -0400

    - [Java 9 中的字符串（String）压缩](https://www.ossez.com/t/java-9-string/14024)

--- core-java-modules/core-java-strings/src/main/java/com/ossez/java9/compactstring/CompactStringDemo.java
+++ core-java-modules/core-java-strings/src/main/java/com/ossez/java9/compactstring/CompactStringDemo.java
@@ -9,17 +9,13 @@ public class CompactStringDemo {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        List strings = IntStream.rangeClosed(1, 10_000_000)
-          .mapToObj(Integer::toString).collect(toList());
+        List strings = IntStream.rangeClosed(1, 10_000_000).mapToObj(Integer::toString).collect(toList());
         long totalTime = System.currentTimeMillis() - startTime;
-        System.out.println("Generated " + strings.size() + " strings in "
-          + totalTime + " ms.");
+        System.out.println("Generated " + strings.size() + " strings in " + totalTime + " ms.");
 
         startTime = System.currentTimeMillis();
-        String appended = (String) strings.stream().limit(100_000)
-          .reduce("", (left, right) -> left.toString() + right.toString());
+        String appended = (String) strings.stream().limit(100_000).reduce("", (left, right) -> left.toString() + right.toString());
         totalTime = System.currentTimeMillis() - startTime;
-        System.out.println("Created string of length " + appended.length()
-          + " in " + totalTime + " ms.");
+        System.out.println("Created string of length " + appended.length() + " in " + totalTime + " ms.");
     }
 }
