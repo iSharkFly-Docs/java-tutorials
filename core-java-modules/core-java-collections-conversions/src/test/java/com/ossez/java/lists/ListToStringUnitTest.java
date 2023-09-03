c9f430ae03d38c1c738ebda32a400a5db61dfb4b
YuCheng Hu <huyuchengus@gmail.com>
2023-09-03 10:31:46 -0400

    Add Java Image process

--- core-java-modules/core-java-collections-conversions/src/test/java/com/ossez/java/lists/ListToStringUnitTest.java
+++ core-java-modules/core-java-collections-conversions/src/test/java/com/ossez/java/lists/ListToStringUnitTest.java
@@ -2,9 +2,12 @@ package com.ossez.java.lists;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.junit.Test;
 
 public class ListToStringUnitTest {
@@ -27,5 +30,28 @@ public class ListToStringUnitTest {
     public void whenStringUtilsJoin_thenPrintCustom() {
         List<Integer> intList = Arrays.asList(1, 2, 3);
        System.out.println(StringUtils.join(intList, "|"));
+
+//        Object obj1  = new Integer(100);
+//        System.out.println(obj1.toString());
+
+//        ToStringBuilder.reflectionToString(intList);
+
+        Country country = new Country("United States", "North America", 4000000);
+        System.out.println(ReflectionToStringBuilder.toString(intList));
+        }
+
+
+
+    class Country {
+        private String name;
+        private String continent;
+        private int population;
+
+        public Country(String name, String continent, int population) {
+            this.name = name;
+            this.continent = continent;
+            this.population = population;
+        }
     }
+
 }
