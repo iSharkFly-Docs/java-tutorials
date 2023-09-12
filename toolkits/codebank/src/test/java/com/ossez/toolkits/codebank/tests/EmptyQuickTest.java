50c63df0e811cf41768c334950c9e1d4a414c54d
YuCheng Hu <huyuchengus@gmail.com>
2023-09-12 01:00:33 -0400

    About the test, we update String Vowel Test

--- toolkits/codebank/src/test/java/com/ossez/toolkits/codebank/tests/EmptyQuickTest.java
+++ toolkits/codebank/src/test/java/com/ossez/toolkits/codebank/tests/EmptyQuickTest.java
@@ -1,6 +1,7 @@
 package com.ossez.toolkits.codebank.tests;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
