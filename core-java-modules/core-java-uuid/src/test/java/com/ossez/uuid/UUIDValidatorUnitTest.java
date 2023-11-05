bfe15ee708df5e0cecee38246b5dd1b32008ddd4
YuCheng Hu <huyuchengus@gmail.com>
2023-11-04 23:50:27 -0400

    Change to add assertThat and assertThatThrownBy

--- core-java-modules/core-java-uuid/src/test/java/com/ossez/uuid/UUIDValidatorUnitTest.java
+++ core-java-modules/core-java-uuid/src/test/java/com/ossez/uuid/UUIDValidatorUnitTest.java
@@ -6,24 +6,31 @@ import org.junit.jupiter.api.Test;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class UUIDValidatorUnitTest {
 
-  @Test
-  public void whenValidUUIDStringIsValidated_thenValidationSucceeds() {
-    String validUUID = "26929514-237c-11ed-861d-0242ac120002";
-    Assertions.assertEquals(UUID.fromString(validUUID).toString(), validUUID);
+    @Test
+    public void whenValidUUIDStringIsValidated_thenValidationSucceeds() {
+        String validUUID = "26929514-237c-11ed-861d-0242ac120002";
+        Assertions.assertEquals(UUID.fromString(validUUID).toString(), validUUID);
+        assertThat(UUID.fromString(validUUID).toString()).isEqualTo(validUUID);
 
-    String invalidUUID = "invalid-uuid";
-    Assertions.assertThrows(IllegalArgumentException.class, () -> UUID.fromString(invalidUUID));
-  }
+        String invalidUUID = "invalid-uuid";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> UUID.fromString(invalidUUID));
+        assertThatThrownBy(() -> {
+            UUID.fromString(invalidUUID);
+        }).isInstanceOf(IllegalArgumentException.class);
 
-  @Test
-  public void whenUUIDIsValidatedUsingRegex_thenValidationSucceeds() {
-    Pattern UUID_REGEX =
-        Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+    }
 
-    Assertions.assertTrue(UUID_REGEX.matcher("26929514-237c-11ed-861d-0242ac120002").matches());
+    @Test
+    public void whenUUIDIsValidatedUsingRegex_thenValidationSucceeds() {
+        Pattern UUID_REGEX = Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
 
-    Assertions.assertFalse(UUID_REGEX.matcher("invalid-uuid").matches());
-  }
+        Assertions.assertTrue(UUID_REGEX.matcher("26929514-237c-11ed-861d-0242ac120002").matches());
+
+        Assertions.assertFalse(UUID_REGEX.matcher("invalid-uuid").matches());
+    }
 }
