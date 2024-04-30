daf9db188df526eaeb71e3083b831d0591d9a2c5
YuCheng Hu <huyuchengus@gmail.com>
2024-04-30 12:18:25 -0400

    删除编译错误

--- persistence-modules/spring-boot-persistence-h2-console/src/main/java/com/ossez/h2db/springboot/daos/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ossez.h2db.springboot.daos;
-
-import com.ossez.h2db.springboot.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
